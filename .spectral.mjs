61639b1505e8030778399f7a1cc8b59861dd26e2
Nicholas Nogradi <nicholas.nogradi@smartbear.com>
2024-07-30 14:49:28 -0400

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NDIyMg/spectral.js?branch_slug=main&token=e6a77664-a119-471f-9617-525bc4e2854b";
+export default { extends: ruleset };
